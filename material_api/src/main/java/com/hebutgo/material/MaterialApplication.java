0a5cf2df87b86ea2f1a252a087b533886aff55a4
shange <767234318@qq.com>
2019-09-04 20:03:10 +0800

    最新一次修改，别动了。

--- material_api/src/main/java/com/hebutgo/material/MaterialApplication.java
+++ material_api/src/main/java/com/hebutgo/material/MaterialApplication.java
@@ -2,7 +2,7 @@ package com.hebutgo.material;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//jjjj
 @SpringBootApplication
 public class MaterialApplication {
 
