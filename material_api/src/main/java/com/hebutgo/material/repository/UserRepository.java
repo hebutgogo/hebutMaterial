ce8177ec31e7b213835667adb9652fafc370b772
yangsong-web <54090202+yangsong-web@users.noreply.github.com>
2019-09-10 22:17:41 +0800

    创建了几个类，然后回去跑步洗澡了

--- material_api/src/main/java/com/hebutgo/material/repository/UserRepository.java
+++ material_api/src/main/java/com/hebutgo/material/repository/UserRepository.java
@@ -2,5 +2,5 @@ package com.hebutgo.material.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public class interface  UserRepository extends JpaRepository<User,Integer> {
+public interface   UserRepository    extends JpaRepository<User,Integer> {
 }
